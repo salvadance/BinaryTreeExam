e4399769b5ce9f3a2fa537ef5a9ecbd7d8de535b
Salvador <88260866+salvadance@users.noreply.github.com>
2026-02-10 15:28:07 -0800

    Add default constructor to InfoExtractor class
    Added a default constructor for InfoExtractor to initialize values to null and height to -1.

--- src/binaryTreeExam/InfoExtractor.java
+++ src/binaryTreeExam/InfoExtractor.java
@@ -1,9 +1,10 @@
 package binaryTreeExam;
 
 /**
- * Class to get information about a binary tree node and encapsulating the node class.
+ * Class to get information about a binary tree node and encapsulating the Node class.
  * This was done to not give outside access to the Node class. Because family references can be changed which will break the binary tree
  * Has read only access once constructed. All fields are final once object is constructed with fields.
+ * 
  */
 public class InfoExtractor {
 	final private Integer VALUE;
@@ -28,27 +29,44 @@ public class InfoExtractor {
 		HEIGHT = height;
 		
 	}
-	
+
+	/**
+	 * Default construct for InfoExtractor will have in a binary tree
+	*/
+	public InfoExtractor() {
+		VALUE = null;
+		PARENT_VALUE = null;
+		LEFT_VALUE = null;
+		RIGHT_VALUE = null;
+		HEIGHT = -1;
+	}
+
+	// Get its value including null
 	public Integer getValue() {
 		return VALUE;
 	}
-	
+
+	// Gets it parent's value including null
 	public Integer getParent() {
 		return PARENT_VALUE;
 	}
-	
+
+	// Get its left child's value including null
 	public Integer getLeftChildValue() {
 		return LEFT_VALUE;
 	}
-	
+
+	// Get its right child value including null
 	public Integer getRightChildValue() {
 		return RIGHT_VALUE;
 	}
-	
+
+	// Get its subtree's height
 	public int getSubtreeHeight() {
 		return HEIGHT;
 	}
-	
+
+	// Checks if VALUE is null therefore making the node null;
 	public boolean isEmpty() {
 		return (VALUE == null);
 	}
